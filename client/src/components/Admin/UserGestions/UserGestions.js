d4df2d8f18d385699ead694c13d0d58ac8098b5d
Finoana Randri <110087713+FinoanaRandria@users.noreply.github.com>
2023-07-10 13:26:49 +0200

    admin part 1

--- client/src/components/Admin/UserGestions/UserGestions.js
+++ client/src/components/Admin/UserGestions/UserGestions.js
@@ -4,239 +4,62 @@ function UserGestions() {
   return (
     <div>
 
-<>
-  <section className="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-20">
-    <h1 className="text-xl font-bold text-white capitalize dark:text-white">
-      Account settings
-    </h1>
-    <form>
-      <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-        <div>
-          <label className="text-white dark:text-gray-200" htmlFor="username">
-            Username
-          </label>
-          <input
-            id="username"
-            type="text"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label
-            className="text-white dark:text-gray-200"
-            htmlFor="emailAddress"
-          >
-            Email Address
-          </label>
-          <input
-            id="emailAddress"
-            type="email"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label className="text-white dark:text-gray-200" htmlFor="password">
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label
-            className="text-white dark:text-gray-200"
-            htmlFor="passwordConfirmation"
-          >
-            Password Confirmation
-          </label>
-          <input
-            id="passwordConfirmation"
-            type="password"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label
-            className="text-white dark:text-gray-200"
-            htmlFor="passwordConfirmation"
-          >
-            Color
-          </label>
-          <input
-            id="color"
-            type="color"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label
-            className="text-white dark:text-gray-200"
-            htmlFor="passwordConfirmation"
-          >
-            Select
-          </label>
-          <select className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
-            <option>Surabaya</option>
-            <option>Jakarta</option>
-            <option>Tangerang</option>
-            <option>Bandung</option>
-          </select>
-        </div>
-        <div>
-          <label
-            className="text-white dark:text-gray-200"
-            htmlFor="passwordConfirmation"
-          >
-            Range
-          </label>
-          <input
-            id="range"
-            type="range"
-            className="block w-full py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label
-            className="text-white dark:text-gray-200"
-            htmlFor="passwordConfirmation"
-          >
-            Date
-          </label>
-          <input
-            id="date"
-            type="date"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label
-            className="text-white dark:text-gray-200"
-            htmlFor="passwordConfirmation"
-          >
-            Text Area
-          </label>
-          <textarea
-            id="textarea"
-            type="textarea"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-            defaultValue={""}
-          />
-        </div>
-        <div>
-          <label className="block text-sm font-medium text-white">Image</label>
-          <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-            <div className="space-y-1 text-center">
-              <svg
-                className="mx-auto h-12 w-12 text-white"
-                stroke="currentColor"
-                fill="none"
-                viewBox="0 0 48 48"
-                aria-hidden="true"
-              >
-                <path
-                  d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                  strokeWidth={2}
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                />
-              </svg>
-              <div className="flex text-sm text-gray-600">
-                <label
-                  htmlFor="file-upload"
-                  className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500"
-                >
-                  <span className="">Upload a file</span>
-                  <input
-                    id="file-upload"
-                    name="file-upload"
-                    type="file"
-                    className="sr-only"
+<div className="flex w-full items-center justify-center min-h-screen">
+    <div className="col-span-11">
+      <div className="overflow-auto lg:overflow-visible ">
+        <table className="table text-gray-400 border-separate space-y-11 text-sm">
+          <thead className="bg-gray-800 text-gray-500 w-full">
+            <tr>
+              <th className="p-3">Id</th>
+              <th className="p-3 text-left">Email</th>
+              <th className="p-3 text-left">Nom</th>
+              <th className="p-3 text-left">Adresse</th>
+              <th className="p-3 text-left">Pays</th>
+              <th className="p-3 text-left">Code Postal</th>
+              <th className="p-3 text-left">Ville</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr className="bg-gray-800">
+              <td className="p-3">
+                <div className="flex align-items-center">
+                  <img
+                    className="rounded-full h-12 w-12  object-cover"
+                    src="https://images.unsplash.com/photo-1613588718956-c2e80305bf61?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=634&q=80"
+                    alt="unsplash image"
                   />
-                </label>
-                <p className="pl-1 text-white">or drag and drop</p>
-              </div>
-              <p className="text-xs text-white">PNG, JPG, GIF up to 10MB</p>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex justify-end mt-6">
-        <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-pink-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600">
-          Save
-        </button>
-      </div>
-    </form>
-  </section>
-  <section className="max-w-4xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800 mt-20">
-    <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">
-      Account settings
-    </h2>
-    <form>
-      <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
-        <div>
-          <label
-            className="text-gray-700 dark:text-gray-200"
-            htmlFor="username"
-          >
-            Username
-          </label>
-          <input
-            id="username"
-            type="text"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label
-            className="text-gray-700 dark:text-gray-200"
-            htmlFor="emailAddress"
-          >
-            Email Address
-          </label>
-          <input
-            id="emailAddress"
-            type="email"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label
-            className="text-gray-700 dark:text-gray-200"
-            htmlFor="password"
-          >
-            Password
-          </label>
-          <input
-            id="password"
-            type="password"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-        <div>
-          <label
-            className="text-gray-700 dark:text-gray-200"
-            htmlFor="passwordConfirmation"
-          >
-            Password Confirmation
-          </label>
-          <input
-            id="passwordConfirmation"
-            type="password"
-            className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
-          />
-        </div>
-      </div>
-      <div className="flex justify-end mt-6">
-        <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600">
-          Save
-        </button>
-      </div>
-    </form>
-  </section>
-</>
+                  <div className="ml-3">
+                    <div className="">Appple</div>
+                    <div className="text-gray-500">mail@rgmail.com</div>
+                  </div>
+                </div>
+              </td>
+              <td className="p-3">Technology</td>
+              <td className="p-3 font-bold">200.00$</td>
+              <td className="p-3 font-bold">200.00$</td>
+              <td className="p-3 font-bold">200.00$</td>
+              <div className="ml-3">
+                    <div className="">Appple</div>
+                    <div className="text-gray-500">mail@rgmail.com</div>
+                  </div>
+              <td className="p-3 font-bold">200.00$</td>
 
+            </tr>
+            
+            
+          </tbody>
+        </table>
+      </div>
+    </div>
+  </div>
+
+
+  <style
+    dangerouslySetInnerHTML={{
+      __html:
+        "\n\t.table {\n\t\tborder-spacing: 0 15px;\n\t}\n\n\ti {\n\t\tfont-size: 1rem !important;\n\t}\n\n\t.table tr {\n\t\tborder-radius: 20px;\n\t}\n\n\ttr td:nth-child(n+7),\n\ttr th:nth-child(n+7) {\n\t\tborder-radius: 0 .625rem .625rem 0;\n\t}\n\n\ttr td:nth-child(1),\n\ttr th:nth-child(1) {\n\t\tborder-radius: .625rem 0 0 .625rem;\n\t}\n"
+    }}
+  />
 
     </div>
   )
--- client/src/pages/Admin/Admin.js
+++ client/src/pages/Admin/Admin.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import Sidebar from '../../components/Admin/Sidebar/Sidebar'
+import UserGestions from '../../components/Admin/UserGestions/UserGestions'
 const Admin = () => {
   return (
-    <div>
-      <Sidebar/>
-       
+    <div className='w-full flex flex-row justify-center content-center h-[600px] mt-7'>
+       <div>
+       <Sidebar/>
+       </div>
+
+        <div>
+        <UserGestions/>
+        </div>
+      
+      
     </div>
   )
 }
